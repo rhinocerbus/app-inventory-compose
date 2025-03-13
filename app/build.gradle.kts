d0350ee8424ca1b5cc11ef95495a770dbc429000
rhinocerbus <m.rubright.dev@gmail.com>
2025-03-13 14:03:49 -0400

    cleanup & organize gradle dependencies & toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,20 +50,22 @@ android {
 }
 
 dependencies {
-	// android/androidx/compose
+	// core
 	implementation(libs.androidx.core.ktx)
+	// compose
+	implementation(platform(libs.androidx.compose.bom))
+	implementation(libs.androidx.compose.ui)
+	implementation(libs.androidx.compose.ui.graphics)
+	debugImplementation(libs.androidx.compose.ui.tooling)
+	implementation(libs.androidx.compose.ui.tooling.preview)
+	implementation(libs.androidx.compose.material3)
+	// androidx/jetpack/compose
+	implementation(libs.androidx.compose.activity)
+	implementation(libs.androidx.compose.navigation)
+	implementation(libs.androidx.compose.livedata)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
 	implementation(libs.androidx.lifecycle.viewmodel.ktx)
-	implementation(libs.androidx.activity.compose)
-	implementation(libs.androidx.ui.graphics.android)
-	implementation(platform(libs.androidx.compose.bom))
-	implementation(libs.androidx.compose.livedata)
-	implementation(libs.androidx.ui)
-	implementation(libs.androidx.ui.graphics)
-	implementation(libs.androidx.ui.tooling.preview)
-	implementation(libs.androidx.material3)
-	implementation(libs.androidx.navigation.compose)
-	debugImplementation(libs.ui.tooling)
+	implementation(libs.androidx.compose.ui.graphics.android)
 	implementation(libs.androidx.compose.constraint)
 
 	// internal libraries (no version necessary)
