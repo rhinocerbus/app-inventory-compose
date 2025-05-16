ef0c7e486d6479e1dfcd6479a19a4521d2ce743a
rhinocerbus <m.rubright.dev@gmail.com>
2025-05-16 12:48:25 -0400

    remove now-duplicate classes

--- app/src/main/java/com/piledrive/inventory/data/model/abstracts/SupaBaseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.piledrive.inventory.data.model.abstracts
-
-import com.squareup.moshi.Json
-
-interface SupaBaseModel {
-	val id: String
-	@Json(name = "created_at")
-	val createdAt: String
-}
